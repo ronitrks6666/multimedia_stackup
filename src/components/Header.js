cc56c78045b4ca4b8e9dca13b8e95a552576c8a3
ronitrks6666 <ronitrks6666@gmail.com>
2023-06-13 10:23:16 +0530

    added features

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export const Header = () => {
+    return (
+        < header style={styles.header} >
+            < h1 style={styles.headerText} > Ronit - initial’s Drive</h1 >
+            < p style={styles.headerSubText} > A File Manager created by Ronit</p >
+        </header >);
+}
+const styles = { header: { backgroundColor: '#333', color: '#fff', textAlign: 'center', padding: '10px' }, headerText: { fontSize: '30px' }, headerSubText: { fontSize: '15px' } }
